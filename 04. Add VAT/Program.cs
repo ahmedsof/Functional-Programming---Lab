8f5e5f79309261066c0c7321d09ad51438ca2c07
Ahmed Softev <ahmed_v@abv.bg>
2020-10-10 14:11:11 +0300

    C#

---
+++ 04. Add VAT/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Add_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal[] nums = Console.ReadLine().Split(", ").Select(decimal.Parse)
+                .Select(x => x * 1.2m).ToArray();
+
+            foreach (var item in nums)
+            {
+                Console.WriteLine($"{item:f2}");
+            }
+        }
+    }
+}
